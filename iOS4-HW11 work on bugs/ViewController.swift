743af89a6842b9345f177c4b4d6b2d6c10fe3262
Aliaksandr Piatrovich <alex.petrovich83@gmail.com>
2021-11-30 21:21:09 +0300

    Initial Commit

---
+++ iOS4-HW11 work on bugs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS4-HW11 work on bugs
+//
+//  Created by Александр Петрович on 30.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
